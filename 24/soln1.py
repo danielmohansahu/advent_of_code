d6a8f7d94a8e42aa0000f874207ca5939b59ddcb
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-12-24 10:36:56 -0500

    Baseline Puzzle #2 working (albeit slowly)

--- 24/soln1.py
+++ 24/soln1.py
@@ -35,10 +35,75 @@ def to_xyz(move):
             import pdb;pdb.set_trace()
     return coords
 
-def process_day(state):
+def process_day(state, origin):
     """ Apply the tile flipping rules to the floor for a single day.
     """
-    return state
+    # create a padded array (simplifies handling of edge cases)
+    current = np.pad(state, 1)
+    origin += np.ones(3,dtype=int)
+
+    # if we have flipped elements on the boundary we'll permanantly pad (another layer)
+    if state[0,:,:].any() or state[-1,:,:].any() or state[:,0,:].any() or state[:,-1,:].any() or state[:,:,0].any() or state[:,:,-1].any():
+        print("Padding results.")
+        current = np.pad(current, 1)
+        origin += np.ones(3,dtype=int)
+
+    # then go through each index and check the rules
+    result = current.copy()
+    processed = np.zeros(result.shape,dtype=bool)
+    hexes = [origin]
+
+    while len(hexes) != 0:
+        new_hexes = []
+
+        for hex_ in hexes:
+            # get the neighbors
+            neighbors = [
+                hex_ + np.array([1,-1,0]),
+                hex_ + np.array([1,0,-1]),
+                hex_ + np.array([0,1,-1]),
+                hex_ + np.array([-1,1,0]),
+                hex_ + np.array([-1,0,1]),
+                hex_ + np.array([0,-1,1])
+            ]
+            # get the state of each of the neighbors
+            flipped = 0
+            for n in neighbors:
+                # check current state
+                idx = tuple(n)
+                if current[idx]:
+                    flipped += 1
+
+                # check if this has been processed; if no, 
+                #  add to the list for the next round (if within bounds)
+                if not processed[idx]:
+                    # needs to be greater than 1 away from the edge
+                    off_boundary = True
+                    for i in range(3):
+                        off_boundary &= (0 < n[i] < result.shape[i]-1)
+                    if off_boundary:
+                        new_hexes.append(n)
+
+            # our rules are:
+            #  If Flipped: change to white if count != 1,2
+            #  If not Flipped: change to black if count == 2
+            idx = tuple(hex_)
+            if current[idx] and flipped not in [1,2]:
+                result[idx] = False
+            elif not current[idx] and flipped == 2:
+                result[idx] = True
+
+            # mark as processed
+            processed[idx] = True
+
+        # update our processing list
+        print(len(new_hexes))
+        hexes = new_hexes
+
+    # remove the padding and return
+    result = result[1:-1,1:-1,1:-1]
+    origin -= np.ones(3,dtype=int)
+    return result, origin
 
 if __name__ == "__main__":
     # load data
@@ -69,6 +134,8 @@ if __name__ == "__main__":
 
     # part #2: evaluate the state day by day
 
-    # first we convert to a more useful format
+    for i in range(100):
+        state, origin = process_day(state, origin)
+        print("Day {}: {}".format(i+1,np.sum(state)))
     
 
