5c49a1cf7edf54aba7d1f4dd29ef9b6b8eb21b37
Daniel M. Sahu <danielmohansahu@gmail.com>
2021-12-04 08:29:37 -0500

    Initial commit of correct answer for Part 1.

--- 2021/four/src/main.rs
+++ 2021/four/src/main.rs
@@ -8,7 +8,7 @@ use std::io::{BufRead, BufReader};
 // const FILENAME : &str = "example.txt";
 const FILENAME : &str= "cards.txt";
 
-fn parse_cards(filename : &str, cards : &mut Vec<Vec<u8>>, turns : &mut Vec<u8>) -> std::io::Result<()> {
+fn parse_cards(filename : &str, cards : &mut Vec<Vec<(u8,bool)>>, turns : &mut Vec<u8>) -> std::io::Result<()> {
     // extract the turn order and set of cards from the given file
 
     // set up a reader and file object
@@ -40,7 +40,7 @@ fn parse_cards(filename : &str, cards : &mut Vec<Vec<u8>>, turns : &mut Vec<u8>)
             // otherwise we want to append this value to our previous card
             for element in line.split_whitespace() {
                 let element : u8 = element.parse().expect("Got non-digit input.");
-                cards.last_mut().expect("Missing last element").push(element);
+                cards.last_mut().expect("Missing last element").push((element, false));
             }
         }
         // println!("Current line: {}", line);
@@ -49,13 +49,71 @@ fn parse_cards(filename : &str, cards : &mut Vec<Vec<u8>>, turns : &mut Vec<u8>)
     Ok(())
 }
 
+fn mark_and_eval(card : &mut Vec<(u8,bool)>, turn : u8) -> bool {
+    // return true if the given card is a winner after
+    // the given turn order, false otherwise.
+    // winner conditions: any column or row is all marked
+
+    // iterate through the given card, marking any elements that match
+    for tup in &mut card.iter_mut() {
+        if tup.0 == turn {
+            assert!(!tup.1);
+            tup.1 = true;
+            // I think we can break here? There shouldn't be any duplicates, right?
+        }
+    }
+
+    // evaluate whether or not this card is a winner
+    // I know. I'm ashamed of it too.
+    card[0..5].iter().all(|&i| i.1 == true)
+        || card[5..10].iter().all(|&i| i.1 == true) 
+        || card[10..15].iter().all(|&i| i.1 == true) 
+        || card[15..20].iter().all(|&i| i.1 == true) 
+        || card[20..25].iter().all(|&i| i.1 == true) 
+        || (card[0].1 && card[5].1 && card[10].1 && card[15].1 && card[20].1)
+        || (card[1].1 && card[6].1 && card[11].1 && card[16].1 && card[21].1)
+        || (card[2].1 && card[7].1 && card[12].1 && card[17].1 && card[22].1)
+        || (card[3].1 && card[8].1 && card[13].1 && card[18].1 && card[23].1)
+        || (card[4].1 && card[9].1 && card[14].1 && card[19].1 && card[24].1)
+}
+
+fn score(card : & Vec<(u8,bool)>, turn : u8) -> u32 {
+    // convenience type conversion
+    let turn = turn as u32;
+
+    // calculate the score of the (winning, presumably) card
+    let mut unmarked_sum : u32 = 0;
+    for element in card {
+        if !element.1 {
+            unmarked_sum += element.0 as u32;
+        }
+    }
+
+    println!("{} * {} = {}", unmarked_sum, turn, unmarked_sum * turn);
+    unmarked_sum * turn
+}
+
 fn main() {
     // extract card information
-    let mut cards : Vec<Vec<u8>> = Vec::new();
+    let mut cards : Vec<Vec<(u8,bool)>> = Vec::new();
     let mut turns : Vec<u8> = Vec::new();
     parse_cards(FILENAME, &mut cards, &mut turns).expect("Unable to parse input.");
 
     println!("Playing BINGO from input {}", FILENAME);
+
+    // iterate through the turn order, starting from turn 5, until we get a winner
+    let mut winner_found = false;
+    for turn in turns {
+        if !winner_found {
+            for card in &mut cards {
+                if mark_and_eval(card, turn) {
+                    score(card, turn);
+                    winner_found = true;
+                    break;
+                }
+            }
+        }
+    }
 
 
 
