e4235a0d50961389b348a48f3df628a48f0a0c7e
Daniel M. Sahu <danielmohansahu@gmail.com>
2021-12-07 06:51:03 -0500

    Added a brute force method for Part 2.

--- 2021/seven/src/main.rs
+++ 2021/seven/src/main.rs
@@ -5,7 +5,7 @@ use std::vec::Vec;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-// const FILENAME: &str = "example.txt";
+ // const FILENAME: &str = "example.txt";
 const FILENAME: &str = "positions.txt";
 
 // parse the input file to get starting positions
@@ -27,6 +27,20 @@ fn parse_input(filename: &str) -> Vec<u64> {
     return result;
 }
 
+fn calculate_cost(positions: &[u64], dest: u64) -> u64 {
+    // returns the cost it would take to move to the given destination
+
+    let mut cost = 0;
+    for pos in positions {
+        let steps = ((dest as i64) - (*pos as i64)).abs() as u64;
+        for step in 1..(steps+1) {
+            cost += step;
+        }
+    }
+
+    cost
+}
+
 fn main() {
     // get starting positions
     let mut positions = parse_input(FILENAME);
@@ -41,5 +55,18 @@ fn main() {
         cost += ((median as i64) - (*pos as i64)).abs();
     }
 
-    println!("Optimum position is {} (costs {})", median, cost);
+    println!("Optimum position for Part A is {} (costs {})", median, cost);
+
+    // for part B we brute force things
+    let mut min_cost = u64::MAX;
+    let mut target = 0;
+    for tgt in positions[0]..(positions[positions.len()-1]+1) {
+        let cost = calculate_cost(&positions, tgt);
+        if cost < min_cost {
+            min_cost = cost;
+            target = tgt;
+        }
+    }
+
+    println!("Optimum position for Part B is {} (costs {})", target, min_cost);
 }
